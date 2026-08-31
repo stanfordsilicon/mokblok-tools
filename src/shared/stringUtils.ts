b2ad33b121741b3f73818c3267c897ee03c2a58f
Conrad Nied <conrad.logos@gmail.com>
2026-08-31 14:53:56 -0700

    Fix some titlecases in demos

--- src/shared/stringUtils.ts
+++ src/shared/stringUtils.ts
@@ -65,3 +65,11 @@ export function getFraktur(text: string): string {
     .map((char) => frakturMap.get(char) || char)
     .join('');
 }
+
+export function titlecase(text: string): string {
+  if (!text) return '';
+  return text
+    .split(' ')
+    .map((word) => word[0].toUpperCase() + word.slice(1).toLowerCase())
+    .join(' ');
+}
